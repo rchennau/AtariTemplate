6998f2da0ffdb0579dc51f202437c9f6cd2a7ce2
Richard Chennault <rchennau@users.noreply.github.com>
2021-01-26 11:10:29 -0800

    Removed color cycling code and reduce to be just Hello World

--- src/hello.c
+++ src/hello.c
@@ -8,23 +8,19 @@ extern const char text[]; /* Defined In text.s file.  Benefit is to externalize 
 
 #pragma static-locals (1)  // Declare local variables as static and add them to the zero-page register of the target system
 
-int i = 0;
+int i = 0; 
 
 int main (void) {
 
 // setup visuals
 
 bgcolor(COLOR_BLACK);	// set the background color
-POKEW(709,56);		// set text color.  708 is memory address for color reg # 0.  
+POKEW(710,192);		// set text color.  710 is memory address for color reg # 2.  
 clrscr();		// clear the screen
 
 	for (i = 0; i <= 254; i++) {
-		printf("%s %i\t", text, i);
-		POKEW(710,i);		// set text color.  708 is memory address for color reg # 0.  
+		printf("%s\n", text);
 		sleep(1);   // x seconds
 	}
-	POKEW(710,192);		// set text color.  710 is memory address for color reg # 2.  This should be green on black  
-	printf("%s 192\n", text);
-	sleep(10);   // 10 seconds
 	return EXIT_SUCCESS;
 }
